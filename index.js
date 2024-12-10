bd2dce47efe12a02971db39768efd52863d37db4
AirFish123 <Christopherherrey@gmail.com>
2024-12-10 08:30:23 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+"O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g",
+"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%",
+"^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",
+".","?","/"];
+
+let passwordLength = 15
+
+
+let themeBtn = document.querySelector(".theme-button")
+
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+
+let copyMessage = document.getElementById("copy-password-text")
+
+let generatePassword = document.getElementById("generate-password-button")
+
+generatePassword.textContent = "GENERATE PASSWORDS"
+copyMessage.textContent = ""
+
+passwordOne.textContent = ""
+passwordTwo.textContent = ""
+
+themeBtn.textContent = "Dark mode"
+
+
+function modeDarkLight() {
+    let bodyDiv = document.querySelector(".body-div")
+    bodyDiv.classList.toggle("body-div-dark-mode")
+
+    if (themeBtn.textContent === "Dark mode"){
+    themeBtn.textContent = "Light mode"
+    } else {themeBtn.textContent = "Dark mode"}
+
+    let paragraph = bodyDiv.querySelector("p")
+    paragraph.classList.toggle("p-dark-mode")
+
+    let paragraph2 = document.getElementById("copy-password-text")
+    paragraph2.classList.toggle("p-dark-mode")
+
+    let toggleDark = bodyDiv.querySelector(".theme-button")
+    toggleDark.classList.toggle("theme-button-dark")
+
+    let passwordBorder = bodyDiv.querySelector(".generated-password-block")
+    passwordBorder.classList.toggle("generated-password-block-dark")
+}
+
+function generatePasswords() {
+    copyMessage.textContent = "Click on password to copy"
+    passwordOne.textContent = ""
+    passwordTwo.textContent = ""
+    generateRandomPassword()
+
+    generatePassword.style.backgroundColor = "#065F46";
+    setTimeout(() => {
+        generatePassword.removeAttribute("style");
+    }, 100);
+    
+}
+// Click on password to copy
+
+function generateRandomPassword() {
+    for (i = 0; i < passwordLength; i++) {
+        let passwordOneCharacter = Math.floor(Math.random()*characters.length)
+        let passwordTwoCharacter = Math.floor(Math.random()*characters.length)
+        passwordOne.textContent += characters[passwordOneCharacter]
+        passwordTwo.textContent += characters[passwordTwoCharacter]
+
+    }
+}
+
+function copyPasswordOne() {
+    if (passwordOne.textContent.trim() !== "") {
+    copyMessage.textContent = "Copied to clipboard!"
+    navigator.clipboard.writeText(passwordOne.innerText);
+
+    passwordOne.style.backgroundColor = "black";
+    setTimeout(() => {
+        passwordOne.removeAttribute("style");
+    }, 100);
+
+    setTimeout(() => {
+        copyMessage.textContent ="Click on password to copy";
+    }, 800);
+
+    // passwordOne.style.marginLeft = "300px"
+    // passwordOne.style.transition = "1s"
+    }
+}
+
+function copyPasswordTwo() {
+
+    if (passwordTwo.textContent.trim() !== "") {
+    copyMessage.textContent = "Copied to clipboard!"
+    navigator.clipboard.writeText(passwordTwo.innerText);
+
+    passwordTwo.style.backgroundColor = "black";
+    setTimeout(() => {
+        passwordTwo.removeAttribute("style");
+    }, 100);
+
+    setTimeout(() => {
+        copyMessage.textContent ="Click on password to copy";
+    }, 800);
+
+    }
+
+
+
+}
